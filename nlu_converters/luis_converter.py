cc42c378521667405e0261348fd10c93741add7f
Hari Krishna Akurathi <hari.akurathi@bshg.com>
2018-04-13 17:51:30 +0200

    Upgraded LUIS schema to 2.2.0 from 1.3.0

--- nlu_converters/luis_converter.py
+++ nlu_converters/luis_converter.py
@@ -1,32 +1,41 @@
 from converter import *
 
 class LuisConverter(Converter):
-	LUIS_SCHEMA_VERSION = "1.3.0"
-			
+	LUIS_SCHEMA_VERSION = "2.2.0"
+
 	def __init__(self):
 		super(LuisConverter, self).__init__()
 		self.bing_entities = set()
-	
-	
+
 	def __add_intent(self, intent):
 		self.intents.add(intent)
-				
+
 	def __add_entity(self, entity):
 		self.entities.add(entity)
-	
+
 	def __add_bing_entity(self, entity):
-		self.bing_entities.add(entity)	
-	
-	def __add_utterance(self, sentence):		
-		entities = []		
+		self.bing_entities.add(entity)
+
+	def __add_utterance(self, sentence):
+		entities = []
 		for e in sentence.entities:
-			entities.append({"entity":e["entity"],"startPos":e["start"],"endPos":e["stop"]})
+			#Calculate the position based on character count.
+			words = (sentence.text).split(' ')
+			index = 0
+			for i in range(len(words)):
+				if i == e["start"]:
+					start_index = index
+					end_index = index + len(words[i])
+				elif i == e["stop"]:
+					end_index = index + len(words[i])
+					break
+				index = index + len(words[i]) + 1
+			#print start_index, end_index
+			entities.append({"entity": e["entity"], "startPos": start_index, "endPos": end_index-1})
 		self.utterances.append({"text": sentence.text, "intent": sentence.intent, "entities": entities})
-	
-	
+
 	def import_corpus(self, file):
 		data = json.load(open(file))
-		
 		#meta data
 		self.name = data["name"]
 		self.desc = data["desc"]
@@ -50,15 +59,18 @@ class LuisConverter(Converter):
 	def export(self, file):
 		luis_json = {}
 		luis_json["luis_schema_version"] = self.LUIS_SCHEMA_VERSION 
+		luis_json["versionId"] = "0.1.0"
 		luis_json["name"] = self.name
 		luis_json["desc"] = self.desc
 		luis_json["culture"] = self.lang  
 		luis_json["intents"] = self.array_to_json(self.intents)
 		luis_json["entities"] = self.array_to_json(self.entities)
 		luis_json["bing_entities"] = self.array_to_json(self.bing_entities)
-		luis_json["actions"] = []
 		luis_json["model_features"] = []
 		luis_json["regex_features"] = []
+		luis_json["regex_entities"] = []
+		luis_json["composites"] = []
+		luis_json["closedLists"] = []
 		luis_json["utterances"] = self.utterances
-
-		self.write_json(file, luis_json)
+		json_data = json.dumps(luis_json, indent=4, sort_keys=True)
+		self.write_json(file, json_data)
